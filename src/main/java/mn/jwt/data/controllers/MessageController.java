e0523f95c9ad500614f5beaf6b2c0cc965758063
Ruben Mondejar <ruben.mondejar@urv.cat>
2020-04-15 09:47:43 +0200

    feat: extractAuthUser

--- src/main/java/mn/jwt/data/controllers/MessageController.java
+++ src/main/java/mn/jwt/data/controllers/MessageController.java
@@ -2,9 +2,6 @@ package mn.jwt.data.controllers;
 
 import io.reactivex.Single;
 
-import com.nimbusds.jwt.JWTParser;
-import com.nimbusds.jwt.SignedJWT;
-
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
@@ -14,6 +11,7 @@ import io.micronaut.security.rules.SecurityRule;
 
 import mn.jwt.data.dtos.MessageDto;
 import mn.jwt.data.services.MessageService;
+import mn.jwt.data.services.UserService;
 
 import java.util.List;
 
@@ -22,9 +20,11 @@ import java.util.List;
 public class MessageController {
 
     private final MessageService messageService;
+    private final UserService userService;
 
-    public MessageController(MessageService messageService) {
+    public MessageController(MessageService messageService, UserService userService) {
         this.messageService = messageService;
+        this.userService = userService;
     }
 
     @Get("/all")
@@ -38,17 +38,11 @@ public class MessageController {
     @Secured({"ADMIN", "VIEW"})
     public  Single<HttpResponse<List<MessageDto>>> getMessages(@Header String authorization) {
 
-        String username;
-
-        try {
-            SignedJWT jwt = (SignedJWT) JWTParser.parse(authorization.substring(7));
-            username = jwt.getJWTClaimsSet().getSubject();
-
-        } catch (Exception e) {
-            return Single.just(HttpResponse.unauthorized());
-        }
-
-        return Single.just(HttpResponse.ok(messageService.findAllByUsername(username)));
+        return Single.just(
+                userService.extractAuthUser(authorization).map(user ->
+                        HttpResponse.ok(messageService.findAllByUsername(user.getUsername()))
+                    )
+                    .orElse(HttpResponse.unauthorized())
+        );
     }
-
 }
--- src/main/java/mn/jwt/data/services/UserService.java
+++ src/main/java/mn/jwt/data/services/UserService.java
@@ -1,15 +1,22 @@
 package mn.jwt.data.services;
 
+import java.text.ParseException;
 import java.util.Optional;
 import javax.inject.Singleton;
 
+import com.nimbusds.jwt.JWTParser;
+import com.nimbusds.jwt.SignedJWT;
+import lombok.extern.slf4j.Slf4j;
 import mn.jwt.data.domain.User;
 import mn.jwt.data.dtos.UserDto;
 import mn.jwt.data.mappers.UserMapper;
 import mn.jwt.data.repositories.UserRepository;
 
+@Slf4j
 @Singleton
 public class UserService {
+
+    private final int authHeaderLength = "Bearer ".length();
 
     private final UserRepository usersRepository;
     private final UserMapper userMapper;
@@ -26,5 +33,20 @@ public class UserService {
 
     public Optional<UserDto> findUser(String username) {
         return usersRepository.findByUsername(username).map(userMapper::toDto);
+    }
+
+    public Optional<UserDto> extractAuthUser(String authorization) {
+
+        try {
+
+            SignedJWT jwt = (SignedJWT) JWTParser.parse(authorization.substring(authHeaderLength));
+            String username = jwt.getJWTClaimsSet().getSubject();
+
+            return findUser(username);
+
+        } catch (ParseException e) {
+            log.error(e.getMessage());
+        }
+        return Optional.empty();
     }
 }
