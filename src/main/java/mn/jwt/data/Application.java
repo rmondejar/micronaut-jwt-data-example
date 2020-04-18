ba1d1e3e3ccb4b6494fe52f4560747229e0bfaf1
Ruben Mondejar <ruben.mondejar@urv.cat>
2020-04-18 11:23:56 +0200

    feat: swagger + gcp

--- src/main/java/mn/jwt/data/Application.java
+++ src/main/java/mn/jwt/data/Application.java
@@ -1,7 +1,16 @@
 package mn.jwt.data;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 import io.micronaut.runtime.Micronaut;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "mn-jwt-jpa",
+                version = "0.3"
+        )
+)
 public class Application {
 
     public static void main(String[] args) {
